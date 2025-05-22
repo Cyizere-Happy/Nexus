c3b9bcfd4297e81baad124beabe81d56ec8d286e
CYIZERE Happy <happycyizere69@gmail.com>
2025-05-22 16:07:42 +0200

    Update Crud/Dashboard.php

--- Crud/Dashboard.php
+++ Crud/Dashboard.php
@@ -154,7 +154,7 @@ if($result){
             </div>
         </div>
         <div class="footer-bottom">
-            <p>&copy; 2025 Nexus Authentication. All rights reserved.</p>
+            <p>&copy; 2025 Nexus Authentication. All rights reserved</p>
         </div>
     </footer>
 </body>
